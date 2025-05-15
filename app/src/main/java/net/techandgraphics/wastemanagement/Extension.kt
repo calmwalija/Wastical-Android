a1306c8395b8a302a993eb9a18dc4d3408fa42a4
Comfort Mwalija <calmwalija@gmail.com>
2025-05-15 10:38:57 +0200

    Refactor: Enhance Home screen UI and data structures
    This commit refactors the Home screen UI to improve layout and introduce new data structures for displaying information.
    
    - Deleted the `HomePaymentView.kt` file.
    - Introduced new data classes `HomeActionUiModel`, `HomeActivityUiModel`, and `TransactionUiModel` in the `model` package to represent UI data.
    - Added `HomeUtils.kt` to define lists of `HomeActivityUiModel` and `HomeActionUiModel` for use in the UI.
    - Added new drawable assets: `ic_cleaning_bucket.xml`, `ic_file_open.xml`, `ic_help.xml`, `ic_payment.xml`, and `ic_truck.xml`.
    - Added new image assets: `im_tnm_mpamba.jpg`.
    - Extended `Extension.kt` with `toAmount()` function to format numbers as currency.
    - Created `HomeActivityView.kt` to display individual upcoming activities.
    - Modified `HomeScreen.kt`:
      - Removed the deprecated `HomePaymentView`.
      - Implemented `FlowRow` for displaying upcoming activities and quick actions, allowing items to wrap to the next line.
      - Introduced a `LetterView` composable to display initials.
      - Changed "Recent Transactions" title to "Recent Invoices".
      - Added dropdown menu with options for Helpline, Sign Out, and Quit.
    - Modified `HomeTransactionView.kt`:
      - Renamed `Transaction` data class to `TransactionUiModel`.
      - Used `TransactionUiModel` to display transaction details.
      - Changed the icon for transactions to `ic_file_open`.
      - Integrated the `toAmount()` extension function for currency formatting.
    - Modified `HomeQuickActionView.kt` to use `HomeActionUiModel` and improved layout
    
    Signed-off-by: Comfort Mwalija <calmwalija@gmail.com>

--- app/src/main/java/net/techandgraphics/wastemanagement/Extension.kt
+++ app/src/main/java/net/techandgraphics/wastemanagement/Extension.kt
@@ -2,5 +2,9 @@ package net.techandgraphics.wastemanagement
 
 import android.content.Context
 import android.widget.Toast
+import java.text.DecimalFormat
 
 fun Context.toast(message: String) = Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+fun Number.toAmount() = "K${format(this)}"
+private fun format(number: Number): String = DecimalFormat("#,###").format(number)
