8cbc8ed877e4d33d489f342c7544a4e335c6f5cd
Comfort Mwalija <calmwalija@gmail.com>
2025-06-30 13:17:17 +0200

    Refactor: Change `AppUrl` object properties to `val`
    This commit modifies the `AppUrl.kt` file.
    
    - The `const val` properties `DEV_API_URL` and `PROD_API_URL` have been changed to `val` properties named `devApi` and `prodApi` respectively.
    - The `FILE_URL` property now uses `devApi` instead of `DEV_API_URL`.
    - The `appUrl()` function has been updated to use `AppUrl.devApi` and `AppUrl.prodApi` instead of the old constant names.
    
    Signed-off-by: Comfort Mwalija <calmwalija@gmail.com>

--- app/src/main/java/net/techandgraphics/wastemanagement/AppUrl.kt
+++ app/src/main/java/net/techandgraphics/wastemanagement/AppUrl.kt
@@ -1,17 +1,17 @@
 package net.techandgraphics.wastemanagement
 
 object AppUrl {
-  const val DEV_API_URL = "http://${BuildConfig.DEV_API_DOMAIN}:8080/"
-  const val PROD_API_URL = "https://${BuildConfig.PROD_API_DOMAIN}/"
-  const val FILE_URL = "${DEV_API_URL}file/"
+  val devApi = "http://${BuildConfig.DEV_API_DOMAIN}:8080/"
+  val prodApi = "https://${BuildConfig.PROD_API_DOMAIN}/"
+  val FILE_URL = "${devApi}file/"
 }
 
 fun appUrl(): EnvConfig {
   return EnvConfig(
     apiDomain = if (BuildConfig.DEBUG) {
-      AppUrl.DEV_API_URL
+      AppUrl.devApi
     } else {
-      AppUrl.PROD_API_URL
+      AppUrl.prodApi
     },
   )
 }
